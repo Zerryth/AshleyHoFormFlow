9e3a3519ccb372267561ddebb018d384603c3cb7
Ashley Ho <ashley.demei.ho@gmail.com>
2018-09-26 15:21:52 -0700

    Recommitting after deleting repo with exposed Secret. Rotated secret. Bot using FormFlow template + iolayemi's demo code inserted. Override to Customer class in Equal and GetHashCode methods to compare objects

---
+++ Models/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormBot.Models
+{
+    [Serializable]
+    //public class Customer
+    //{
+    //    public Customer(string name)
+    //    {
+    //        Name = name;
+    //        AccountNumber = "123";
+    //    }
+
+    //    public string Name { get; set; }
+    //    public string AccountNumber { get; set; }
+    //}
+
+    public class Customer
+    {
+        public string AccountNumber { get; set; }
+        public string Name { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+            AccountNumber = "123";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var c = obj as Customer;
+
+            if (c == null)
+            {
+                return false;
+            }
+
+            return this.AccountNumber.Equals(c.AccountNumber) && this.Name.Equals(c.Name);
+        }
+        public override int GetHashCode()
+        {
+            return AccountNumber.GetHashCode() ^ Name.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return $"{AccountNumber} {Name}";
+        }
+    }
+
+}
---
+++ Models/DynaForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using FormBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading.Tasks;
+
+namespace FormBot.Models
+{
+    [Serializable]
+    public class DynaForm
+    {
+        public Customer Source { get; set; }
+
+        public Customer Destination { get; set; }
+
+        public static IForm<DynaForm> BuildFormAsync()
+        {
+            List<Customer> allCustomers = new List<Customer>()
+            {
+                new Customer("Harry"),
+                new Customer("Ron"),
+                new Customer("Hermione")
+            };
+
+            try
+            {
+
+            return new FormBuilder<DynaForm>()
+                .Message("Welcome to the simple sandwich order bot!")
+                .Field(new FieldReflector<DynaForm>(nameof(Source))
+                    .SetType(null)
+                    .SetFieldDescription(nameof(Source))
+                    .SetFieldTerms(nameof(Source))
+                    .SetDefine((state, field) => {
+
+                        foreach (var customer in allCustomers)
+                        {
+                            field
+                                .AddDescription(customer, new DescribeAttribute(description: customer.Name))
+                                .AddTerms(customer, customer.Name);
+                        }
+
+                        return Task.FromResult(true);
+                    })
+                    .SetPrompt(new PromptAttribute("Select the source account \n {||} \n")
+                    {
+                        ChoiceStyle = ChoiceStyleOptions.Buttons
+                    })
+                    .SetAllowsMultiple(false)
+                )
+
+                .Field(new FieldReflector<DynaForm>(nameof(Destination))
+                    .SetType(null)
+                    .SetFieldDescription(nameof(Destination))
+                    .SetDependencies(nameof(Destination))
+                    .SetFieldTerms(nameof(Destination))
+                    .SetDefine((state, field) =>
+                    {
+
+                        foreach (var customer in allCustomers)
+                        {
+                            field
+                                .AddDescription(customer, new DescribeAttribute(description: customer.Name))
+                                .AddTerms(customer, customer.Name);
+                        }
+
+                        return Task.FromResult(true);
+                    })
+                    .SetPrompt(new PromptAttribute("Select the destination account \n {||} \n")
+                    {
+                        ChoiceStyle = ChoiceStyleOptions.Buttons
+                    })
+                    .SetAllowsMultiple(false)
+                )
+
+                .Confirm("Do you want to continue? {||} ")
+                .OnCompletion(async (context, order) =>
+                {
+                    await context.PostAsync("Excellent! Your order has been placed. :)");
+                })
+                .Build();
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+                throw;
+            }
+        }
+    }
+}
